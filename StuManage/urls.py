3733042f1881f08b17cc88cd8c35d22fed40ad04
daihong <daihong391@gmail.com>
2015-05-05 22:37:35 -0600

    finish administer add accounts function

--- StuManage/urls.py
+++ StuManage/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from studentManage.views import mainpage, changePasswd, addAccount
+from studentManage.views import mainpage, changePasswd, addAccount1, addAccount2, addAccount3
 import settings,os
 
 urlpatterns = patterns('',
@@ -9,7 +9,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     (r'^mainpage/$', mainpage),
     (r'^changePasswd/$', changePasswd),
-    (r'^addAccount/$', addAccount),
+    (r'^addAccount1/$', addAccount1),
+    (r'^addAccount2/$', addAccount2),
+    (r'^addAccount3/$', addAccount3),
 
     (r'^css/(?P<path>.*)$','django.views.static.serve',
     	{'document_root': os.path.join(os.path.dirname(__file__),'templates/css').replace('\\','/') }
--- studentManage/views.py
+++ studentManage/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from .forms import loginForm
-from studentManage.models import Adminer,Course
+from studentManage.models import Adminer,Course,Teacher,Student
 from django.db.models import Count,Q
 import json as simplejson
 
@@ -49,10 +49,17 @@ def changePasswd(request):
 	return render_to_response('adminPage.html', {'MESSAGE':MESSAGE, 'username':username},context_instance=RequestContext(request))
 
 #add account for administer
-def addAccount(request):
+def addAccount1(request):
 	administerName=request.POST.get('administerName')
 	newPassword=request.POST.get('newPassword11')
 	username=request.POST.get('username')
+
+	#obtain all the courses name
+	courseList=[]
+	courses=Course.objects.all().values_list('courseName', flat=True)
+	for course in courses:
+		courseList.append(course)
+	course_list=simplejson.dumps(courseList)
 	MESSAGE=''
 	#add account
 	if Adminer.objects.filter(username=administerName).count()==0:
@@ -62,4 +69,56 @@ def addAccount(request):
 	else:
 		MESSAGE='Username existed'
 
-	return render_to_response('adminPage.html', {'MESSAGE':MESSAGE, 'username':username},context_instance=RequestContext(request))
+	return render_to_response('adminPage.html', {'MESSAGE1':MESSAGE, 'username':username, 'courseList':course_list},context_instance=RequestContext(request))
+
+#add account for teacher
+def addAccount2(request):
+	teacherId=request.POST.get('TeacherId')
+	teacherName=request.POST.get('TeacherName')
+	courseName=request.POST.get('courseName')
+	teacherPassword=request.POST.get('teacherPassword')
+	username=request.POST.get('username')
+
+	#obtain all the courses name
+	courseList=[]
+	courses=Course.objects.all().values_list('courseName', flat=True)
+	for course in courses:
+		courseList.append(course)
+	course_list=simplejson.dumps(courseList)
+	courseSelect=Course.objects.all().get(courseName=courseName)
+	MESSAGE=''
+	#add account
+	if Teacher.objects.filter(teacherId=teacherId).count()==0:
+		p=Teacher(teacherId=teacherId, teacherName=teacherName, course=courseSelect, teacherPassword=teacherPassword)
+		p.save()
+		MESSAGE='Account Creating'
+	else:
+		MESSAGE='Username existed'
+
+	return render_to_response('adminPage.html', {'MESSAGE2':MESSAGE, 'username':username, 'courseList':course_list},context_instance=RequestContext(request))
+
+#add account for student
+def addAccount3(request):
+	studentId=request.POST.get('studentId')
+	studentName=request.POST.get('studentName')
+	courseName=request.POST.get('courseName')
+	studentPassword=request.POST.get('studentPassword')
+	username=request.POST.get('username')
+
+	#obtain all the courses name
+	courseList=[]
+	courses=Course.objects.all().values_list('courseName', flat=True)
+	for course in courses:
+		courseList.append(course)
+	course_list=simplejson.dumps(courseList)
+	courseSelect=Course.objects.all().get(courseName=courseName)
+	MESSAGE=''
+	#add account
+	if Student.objects.filter(studentId=studentId).count()==0:
+		p=Student(studentId=studentId, studentName=studentName, course=courseSelect, studentPassword=studentPassword)
+		p.save()
+		MESSAGE='Account Creating'
+	else:
+		MESSAGE='Username existed'
+
+	return render_to_response('adminPage.html', {'MESSAGE3':MESSAGE, 'username':username, 'courseList':course_list},context_instance=RequestContext(request))
